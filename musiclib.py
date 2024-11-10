592eafa65c6ad555a3a810a4d3cf095810b37457
utkarshsahil04 <utkarsh.sahil04@gmail.com>
2024-11-10 19:54:07 +0530

    Update musiclib.py

--- musiclib.py
+++ musiclib.py
@@ -4,5 +4,11 @@ music={
     "heat wave":"https://www.youtube.com/watch?v=PIMQX6uxwl8&list=RDU3uKF97Vxu4&index=4&ab_channel=AnimeMVSensei",
     "take away":"https://www.youtube.com/watch?v=wI6T_PioDFA&list=RDU3uKF97Vxu4&index=5&ab_channel=AnimeMVSensei",
     "my":"https://open.spotify.com/playlist/4ym1SKZgqzPmF4nplwvL0P",
-    "2010s Mix":"https://open.spotify.com/playlist/37i9dQZF1EQqedj0y9Uwvu"
+    "2010s Mix":"https://open.spotify.com/playlist/37i9dQZF1EQqedj0y9Uwvu",
+    "Romantic":"https://www.youtube.com/watch?v=3-buUW3gmtU&ab_channel=ZeeMusicCompany",
+    "Sonu nigam":"https://www.youtube.com/watch?v=ILID0u6aZ18&ab_channel=ZeeMusicCompany",
+    "Romantic 2":"https://www.youtube.com/watch?v=7HBA20Y5dT8&ab_channel=T-Series",
+    "Amplifier":"https://youtu.be/AF-xMvW9WF4?si=eIpnhVIdTTGtd3PO",
+    "Blue eyes":"https://www.youtube.com/watch?v=NbyHNASFi6U&list=RDAF-xMvW9WF4&index=2&ab_channel=T-Series",
+    
 }
